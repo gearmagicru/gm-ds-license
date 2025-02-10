0e7c248c309bdd967bf680d1be33dd35e2b747cf
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-10 14:53:48 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'License',
+    '{description}' => 'Web application edition license key',
+
+    // Шаблон
+    'License key' => 'License key',
+    'Key: "{0}"' => 'Key: &laquo;<b>{0}</b>&raquo;',
+    'The key is not installed. You cannot fully use all the functionality of your web application components.' 
+        => '<span>The key is not installed</span>. You cannot fully use all the functionality of your web application components.',
+    'Activate' => 'Activate',
+    'The key has been activated' => 'The key has been activated',
+    'read more' => 'read more'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Этот файл является частью виджета информационной панели (Dashboard).
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Лицензия',
+    '{description}' => 'Лицензионный ключ редакции веб-приложения',
+
+    // Шаблон
+    'License key' => 'Лицензионный ключ',
+    'Key: "{0}"' => 'Ключ: &laquo;<b>{0}</b>&raquo;',
+    'The key is not installed. You cannot fully use all the functionality of your web application components.' 
+        => '<span>Ключ не установлен</span>. Вы не можете полностью использовать весь функционал компонентов Вашего веб-приложения.',
+    'Activate' => 'Активировать',
+    'The key has been activated' => 'Ключ редакции веб-приложения активирован',
+    'read more' => 'подробнее'
+];
